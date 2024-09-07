bab9485047b611f08da17793b61d343d8edd9158
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2024-09-07 15:04:14 +0200

    Let Roxygen generate `paramlink2-package.Rd`

--- R/paramlink2-package.R
+++ R/paramlink2-package.R
@@ -1,15 +1,7 @@
-#' paramlink2: Parametric linkage analysis
-#'
-#' Parametric linkage analysis of monogenic traits in medical pedigrees.
-#' Features include singlepoint analysis, multipoint analysis via 'MERLIN'
-#' (Abecasis et al. (2002) \doi{10.1038/ng786}), visualisation of log of the
-#' odds (LOD) scores and summaries of linkage peaks. Disease models may be
-#' specified to  accommodate phenocopies, reduced penetrance and liability
-#' classes. 'paramlink2' is part of the 'ped suite' package ecosystem, presented
-#' in 'Pedigree Analysis in R' (Vigeland, 2021, ISBN:9780128244302).
-#'
-#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import pedtools
-#'
-#' @name paramlink2
+## usethis namespace: end
 NULL
