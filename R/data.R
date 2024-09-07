5b24f5e9e0df940d339d25a97d7755b14bbcac9f
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2024-09-07 15:09:42 +0200

    Doc tweaks

--- R/data.R
+++ R/data.R
@@ -1,14 +1,17 @@
 #' Dominant linkage analysis example
 #'
-#' A dataset with SNP genotypes for 14 members of a pedigree affected with a dominant disorder.
+#' A dataset with SNP genotypes for 14 members of a pedigree affected with a
+#' dominant disorder.
 #'
 #' @format A list with 3 elements:
 #'
-#' * `ped`: A [pedtools::ped] object describing a pedigree with 19 individuals, including genotypes for 14 members at 248 markers on Chromosome 1.
+#' * `ped`: A [pedtools::ped] object describing a pedigree with 19 individuals,
+#'   including genotypes for 14 members at 248 markers on Chromosome 1.
 #'
 #' * `aff`: A vector indicating the affected pedigree members.
 #'
-#' * `map`: A data frame with 3 columns (`chrom`, `marker`, `cm`) describing the centiMorgan positions of the markers.
+#' * `map`: A data frame with 3 columns (`chrom`, `marker`, `cm`) describing the
+#'   centiMorgan positions of the markers.
 #'
 #' @examples
 #'
